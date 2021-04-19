05faeb4fed5e9899bd3fe351daed338ed962e943
Alfredo Ficocelli <alfredo.ficocelli@gmail.com>
2021-04-19 13:09:08 +0200

    navbar complete

--- dist/script.js
+++ dist/script.js
@@ -5,7 +5,39 @@ var app = new Vue ({
   data:{
 
     logo:"logo.png",
-    
+
+    menu:[
+      {
+        id:1,
+        voice:"Home",
+        new:false
+      },
+      {
+        id:2,
+        voice:"Services",
+        new:false
+      },
+      {
+        id:3,
+        voice:"About",
+        new:false
+      },
+      {
+        id:4,
+        voice:"Videos",
+        new:false
+      },
+      {
+        id:5,
+        voice:"Blog",
+        new:false
+      },
+      {
+        id:6,
+        voice:"Store",
+        new:true
+      },
+    ],
 
   }
 
